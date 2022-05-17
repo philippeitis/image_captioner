87b32ff9c5bccd5f23b47f123daeabc4485671b8
philippeitis <33013301+philippeitis@users.noreply.github.com>
2022-05-16 23:16:11 -0700

    Use internal ip of 0.0.0.0 for image_db

--- src/image_db/src/main.rs
+++ src/image_db/src/main.rs
@@ -10,25 +10,32 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 
-
 use crate::db::{fetch_raw, upload_raw, SQLiteDatabase};
 use crate::images::{fetch_jpg, fetch_png};
-use actix_web::{web, App, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
 
 // TODO: GET /supported_ext: get supported file formats
+
+#[get("/health")]
+pub async fn health() -> HttpResponse {
+    HttpResponse::Ok().body("success")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let uri = std::env::var("DATABASE_URL").expect("Missing DATABASE_URL.");
-    let port = u16::from_str(&std::env::var("IMAGE_DB_PORT").expect("Missing DB port")).expect("DB not u16");
+    let ip = std::env::var("IMAGE_DB_IP").unwrap_or(String::from("127.0.0.1"));
+    let port = u16::from_str(&std::env::var("IMAGE_DB_PORT").expect("Missing DB port"))
+        .expect("DB not u16");
     let data = web::Data::new(Arc::new(
         SQLiteDatabase::open(uri, PathBuf::from("./images"))
             .await
             .expect("Opening database failed"),
     ));
-    println!("Opening application on 127.0.0.8:{}", port);
+    println!("Opening application on {}:{}", ip, port);
     HttpServer::new(move || {
         App::new()
+            .service(health)
             .service(
                 web::resource("/upload_raw")
                     .app_data(data.clone())
@@ -50,7 +57,7 @@ async fn main() -> std::io::Result<()> {
                     .route(web::get().to(fetch_raw)),
             )
     })
-    .bind(("127.0.0.8", port))?
+    .bind((ip, port))?
     .run()
     .await
 }
